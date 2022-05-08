041b5cb42ade01c34918bfe98003da48d95f2711
Alex <sashamarx67@gmail.com>
2022-05-08 15:05:19 +0300

    Second Commit c:

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,7 +2,7 @@ import asyncio
 
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.builtin import CommandStart, Text, Command
+from aiogram.dispatcher.filters.builtin import Text, Command
 from keyboards.inline.next import next_step, last_step
 from loader import dp, bot
 from states.inserting import TryingCatch
@@ -21,6 +21,16 @@ async def start_cmd(message: types.Message):
         #                      'Для продолжения, просто <b>нажми на кнопку</b> ниже ', reply_markup=next_step)
     else:
         await message.answer('Прости, но данный бот был создан только для Гоши!')
+
+
+@dp.message_handler(content_types=types.ContentTypes.VIDEO_NOTE)
+async def get_file_id(note: types.Message):
+    await note.answer(note.video_note.file_id)
+
+
+@dp.message_handler(content_types=types.ContentTypes.STICKER)
+async def get_sticker_id(stick: types.Message):
+    await stick.answer(stick.sticker.file_id)
 
 
 @dp.callback_query_handler(Text(equals='click'))
@@ -44,8 +54,24 @@ async def trying_password(message: types.Message, state: FSMContext):
         await message.answer('Умничка!\n'
                              'Подарок ты увидишь через три секунды :)')
         await state.reset_state()
-        await asyncio.sleep(3)
 
-        # тут кружочки
+        file_id = ['DQACAgIAAxkBAANgYnemlJMsUiGDm5YgujH679Z7mrQAAmAZAAIcVqBLvv0D6hIp3lckBA',
+                   'DQACAgIAAxkBAANeYnemL10L142p-SNwIrFFJFnVJgQAAsQXAALEK7FLQHIbYMB9r7IkBA',
+                   'DQACAgIAAxkBAANiYnemxVeSscBxCD8Rjp_Xmxs5MaYAAtgUAAKoobBL2pNlz9IrlbQkBA',
+                   'DQACAgIAAxkBAANkYnem44wg4JSc92NXr9KT0zi0AlYAAugWAAKkn7lLsNwQLOMgCXQkBA',
+                   'DQACAgIAAxkBAANxYneoR7ypCXLdRWIfGo1uoP-iZvkAAnsZAAKRarlL_8MSxDhYHyQkBA']
+        for note in file_id:
+            await bot.send_video_note(
+                chat_id=message.chat.id,
+                video_note=note
+            )
+            await asyncio.sleep(1)
+
+        await message.answer('Желаем тебе всего самого наилучшего!\n'
+                             'Здаровья, счастья, денег и море удачи!\n\n'
+                             '<b>С любовью, твои кенты</b>')
+        await message.answer_sticker(
+            sticker='CAACAgIAAxkBAAN1Yneo4rIn0V1MgkMkVA7N3yfDhUsAAgodAALBqCBJCI03_mLw0XgkBA'
+        )
     else:
         await message.answer('Жесть.. Неправильно, пробуй еще раз :(')
